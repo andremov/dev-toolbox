86245782cf5b3d548bd38ac6110550a6e3841e12
Andres-XL <amovillaw01@gmail.com>
2025-08-24 23:00:01 -0500

    docs and learning

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -40,24 +40,6 @@ const tagColorMap: Record<Tag, string> = {
 
 const data: Tool[] = [
   {
-    name: "Dev Roadmaps",
-    preview: "/previews/dev-roadmaps.png",
-    description:
-      "A collection of roadmaps for developers to guide their learning and career paths.",
-    link: "https://roadmap.sh",
-    tags: ["dev"],
-    category: "Documentation & Learning",
-  },
-  {
-    name: "Starlight Docs",
-    preview: "/previews/starlight.png",
-    description:
-      "A modern documentation site generator that helps you create beautiful and functional docs.",
-    link: "https://starlight.astro.build",
-    tags: ["dev", "open-source", "docs"],
-    category: "Documentation & Learning",
-  },
-  {
     name: "XY Flow",
     preview: "/previews/react-flow.png",
     description:
@@ -92,24 +74,6 @@ const data: Tool[] = [
     link: "https://stirlingpdf.com",
     tags: ["dev", "open-source"],
     category: "Open Source Tools & Utilities",
-  },
-  {
-    name: "Docmost",
-    preview: "/previews/docmost.png",
-    description:
-      "A tool for creating and managing documentation with a focus on simplicity and usability.",
-    link: "https://docmost.com",
-    tags: ["dev", "open-source", "notes"],
-    category: "Documentation & Learning",
-  },
-  {
-    name: "Obsidian",
-    preview: "/previews/obsidian.png",
-    description:
-      "A powerful knowledge base that works on local Markdown files, allowing you to create and manage your notes and documents with ease.",
-    link: "https://obsidian.md",
-    tags: ["dev", "open-source", "notes"],
-    category: "Documentation & Learning",
   },
   {
     name: "Sonner - Toast Notifications",
@@ -191,15 +155,6 @@ const data: Tool[] = [
     link: "https://github.com/charmbracelet/bubbletea",
     tags: ["dev", "open-source", "pkg", "terminal"],
     category: "Development Libraries & Frameworks",
-  },
-  {
-    name: "Glow - Charm Bracelet",
-    preview: "/previews/glow-charm.png",
-    description:
-      "A terminal-based Markdown reader that allows you to view and navigate Markdown files in your terminal.",
-    link: "https://github.com/charmbracelet/glow",
-    tags: ["dev", "open-source", "pkg", "terminal"],
-    category: "Documentation & Learning",
   },
   {
     name: "Actual Budget",
@@ -345,24 +300,6 @@ const data: Tool[] = [
     category: "Development Libraries & Frameworks",
   },
   {
-    name: "JS Operator Lookup",
-    preview: "/previews/js-operator-lookup.png",
-    description:
-      "A tool for looking up JavaScript operators and learn more about them.",
-    link: "https://www.joshwcomeau.com/operator-lookup/",
-    tags: ["dev", "docs"],
-    category: "Documentation & Learning",
-  },
-  {
-    name: "JS Snippets",
-    preview: "/previews/js-snippets.png",
-    description:
-      "A collection of useful JavaScript snippets for various tasks and functionalities.",
-    link: "https://www.joshwcomeau.com/snippets/",
-    tags: ["dev", "docs"],
-    category: "Documentation & Learning",
-  },
-  {
     name: "Beekeeper Studio",
     preview: "/previews/beekeeper-studio.png",
     description:
@@ -370,24 +307,6 @@ const data: Tool[] = [
     link: "https://github.com/beekeeper-studio/beekeeper-studio",
     tags: ["dev", "open-source"],
     category: "Open Source Tools & Utilities",
-  },
-  {
-    name: "drawDB",
-    preview: "/previews/drawdb.png",
-    description:
-      "A tool for creating and managing database diagrams, making it easy to visualize and design your database schema.",
-    link: "https://drawdb.app",
-    tags: ["dev", "open-source", "docs"],
-    category: "Documentation & Learning",
-  },
-  {
-    name: "ChartDB",
-    preview: "/previews/chartdb.png",
-    description:
-      "A tool for creating and managing charts and visualizations from your database data.",
-    link: "https://chartdb.io",
-    tags: ["dev", "open-source", "docs"],
-    category: "Documentation & Learning",
   },
   {
     name: "Kdenlive",
@@ -415,15 +334,6 @@ const data: Tool[] = [
     link: "https://toolfinder.co",
     tags: ["dev"],
     category: "Productivity & Utilities",
-  },
-  {
-    name: "JavaScript Patterns",
-    preview: "/previews/javascript-patterns.png",
-    description:
-      "A collection of design patterns and best practices for writing clean and maintainable JavaScript code.",
-    link: "https://www.patterns.dev/",
-    tags: ["dev", "docs", "open-source"],
-    category: "Documentation & Learning",
   },
 ];
 
@@ -623,5 +533,98 @@ export const aiAndData: Tool[] = [
     link: "https://github.com/deepseek-ai",
     tags: ["ai", "open-source", "dev"],
     category: "AI & Data",
+  },
+];
+
+export const documentationAndLearning: Tool[] = [
+  {
+    name: "Dev Roadmaps",
+    preview: "/previews/dev-roadmaps.png",
+    description:
+      "A collection of roadmaps for developers to guide their learning and career paths.",
+    link: "https://roadmap.sh",
+    tags: ["dev"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "Starlight Docs",
+    preview: "/previews/starlight.png",
+    description:
+      "A modern documentation site generator that helps you create beautiful and functional docs.",
+    link: "https://starlight.astro.build",
+    tags: ["dev", "open-source", "docs"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "Docmost",
+    preview: "/previews/docmost.png",
+    description:
+      "A tool for creating and managing documentation with a focus on simplicity and usability.",
+    link: "https://docmost.com",
+    tags: ["dev", "open-source", "notes"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "Obsidian",
+    preview: "/previews/obsidian.png",
+    description:
+      "A powerful knowledge base that works on local Markdown files, allowing you to create and manage your notes and documents with ease.",
+    link: "https://obsidian.md",
+    tags: ["dev", "open-source", "notes"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "Glow - Charm Bracelet",
+    preview: "/previews/glow-charm.png",
+    description:
+      "A terminal-based Markdown reader that allows you to view and navigate Markdown files in your terminal.",
+    link: "https://github.com/charmbracelet/glow",
+    tags: ["dev", "open-source", "pkg", "terminal"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "JS Operator Lookup",
+    preview: "/previews/js-operator-lookup.png",
+    description:
+      "A tool for looking up JavaScript operators and learn more about them.",
+    link: "https://www.joshwcomeau.com/operator-lookup/",
+    tags: ["dev", "docs"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "JS Snippets",
+    preview: "/previews/js-snippets.png",
+    description:
+      "A collection of useful JavaScript snippets for various tasks and functionalities.",
+    link: "https://www.joshwcomeau.com/snippets/",
+    tags: ["dev", "docs"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "drawDB",
+    preview: "/previews/drawdb.png",
+    description:
+      "A tool for creating and managing database diagrams, making it easy to visualize and design your database schema.",
+    link: "https://drawdb.app",
+    tags: ["dev", "open-source", "docs"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "ChartDB",
+    preview: "/previews/chartdb.png",
+    description:
+      "A tool for creating and managing charts and visualizations from your database data.",
+    link: "https://chartdb.io",
+    tags: ["dev", "open-source", "docs"],
+    category: "Documentation & Learning",
+  },
+  {
+    name: "JavaScript Patterns",
+    preview: "/previews/javascript-patterns.png",
+    description:
+      "A collection of design patterns and best practices for writing clean and maintainable JavaScript code.",
+    link: "https://www.patterns.dev/",
+    tags: ["dev", "docs", "open-source"],
+    category: "Documentation & Learning",
   },
 ];
