3dcf7c3c60fc8bb50d99239932c133c1d588ff6f
Andres-XL <amovillaw01@gmail.com>
2025-08-24 22:53:22 -0500

    design and ui tools

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -40,15 +40,6 @@ const tagColorMap: Record<Tag, string> = {
 
 const data: Tool[] = [
   {
-    name: "Lucide Icons",
-    preview: "/previews/lucide.png",
-    description:
-      "Lucide is a collection of open-source icons designed to be simple and consistent.",
-    link: "https://lucide.dev",
-    tags: ["icon", "design", "open-source"],
-    category: "Design & UI",
-  },
-  {
     name: "JSON Data AI",
     preview: "/previews/json-data-ai.png",
     description:
@@ -73,23 +64,6 @@ const data: Tool[] = [
     link: "https://roadmap.sh",
     tags: ["dev"],
     category: "Documentation & Learning",
-  },
-  {
-    name: "Mobbin",
-    preview: "/previews/mobbin.png",
-    description:
-      "A design library with a collection of mobile app designs and patterns.",
-    link: "https://mobbin.com",
-    tags: ["design", "typography"],
-    category: "Design & UI",
-  },
-  {
-    name: "Tailwind CSS Colors",
-    preview: "/previews/tailwind-ui-colors.png",
-    description: "A sample website to sample color palettes for Tailwind CSS.",
-    link: "https://uicolors.app/",
-    tags: ["dev", "design"],
-    category: "Design & UI",
   },
   {
     name: "Starlight Docs",
@@ -135,15 +109,6 @@ const data: Tool[] = [
     link: "https://stirlingpdf.com",
     tags: ["dev", "open-source"],
     category: "Open Source Tools & Utilities",
-  },
-  {
-    name: "Carbon",
-    preview: "/previews/carbon.png",
-    description:
-      "A desktop application for creating beautiful images of your source code.",
-    link: "https://carbon.now.sh",
-    tags: ["dev", "design", "open-source", "terminal"],
-    category: "Design & UI",
   },
   {
     name: "Docmost",
@@ -281,24 +246,6 @@ const data: Tool[] = [
     category: "Development Libraries & Frameworks",
   },
   {
-    name: "Tailwind CSS",
-    preview: "/previews/tailwind-css.png",
-    description:
-      "A utility-first CSS framework for creating modern and responsive web designs.",
-    link: "https://tailwindcss.com",
-    tags: ["dev", "design", "open-source"],
-    category: "Design & UI",
-  },
-  {
-    name: "Boring Avatars",
-    preview: "/previews/boring-avatars.png",
-    description:
-      "A collection of simple, customizable avatars for use in web applications.",
-    link: "https://boringavatars.com",
-    tags: ["dev", "design", "open-source"],
-    category: "Design & UI",
-  },
-  {
     name: "Remix",
     preview: "/previews/remix.png",
     description:
@@ -389,15 +336,6 @@ const data: Tool[] = [
     category: "Development Libraries & Frameworks",
   },
   {
-    name: "Lottie Files",
-    preview: "/previews/lottie-files.png",
-    description:
-      "A library for rendering animations created with Adobe After Effects in web applications.",
-    link: "https://github.com/lottiefiles",
-    tags: ["dev", "design", "open-source"],
-    category: "Design & UI",
-  },
-  {
     name: "Franz",
     preview: "/previews/franz.png",
     description:
@@ -424,24 +362,6 @@ const data: Tool[] = [
     category: "Development Libraries & Frameworks",
   },
   {
-    name: "CSS Shadow Palette Generator",
-    preview: "/previews/css-shadow-palette-generator.png",
-    description:
-      "A tool for generating CSS shadow palettes, allowing you to create consistent and visually appealing shadows for your web designs.",
-    link: "https://www.joshwcomeau.com/shadow-palette/",
-    tags: ["dev", "design"],
-    category: "Design & UI",
-  },
-  {
-    name: "CSS Gradient Generator",
-    preview: "/previews/css-gradient-generator.png",
-    description:
-      "A tool for creating and customizing CSS gradients, making it easy to add beautiful gradients to your web designs.",
-    link: "https://www.joshwcomeau.com/gradient-generator/",
-    tags: ["dev", "design"],
-    category: "Design & UI",
-  },
-  {
     name: "JS Operator Lookup",
     preview: "/previews/js-operator-lookup.png",
     description:
@@ -458,42 +378,6 @@ const data: Tool[] = [
     link: "https://www.joshwcomeau.com/snippets/",
     tags: ["dev", "docs"],
     category: "Documentation & Learning",
-  },
-  {
-    name: "SVG Blob generator",
-    preview: "/previews/svg-blob-generator.png",
-    description:
-      "A tool for generating SVG blobs, allowing you to create unique and visually appealing shapes for your web designs.",
-    link: "https://www.blobmaker.app/",
-    tags: ["dev", "design"],
-    category: "Design & UI",
-  },
-  {
-    name: "Unsplash",
-    preview: "/previews/unsplash.png",
-    description:
-      "A platform for finding and sharing high-quality, free-to-use images for your projects.",
-    link: "https://unsplash.com",
-    tags: ["design"],
-    category: "Design & UI",
-  },
-  {
-    name: "Deck Gallery",
-    preview: "/previews/deck-gallery.png",
-    description:
-      "A collection of beautifully designed decks, slides, keynotes, guidelines, etc.",
-    link: "https://deck.gallery",
-    tags: ["design"],
-    category: "Design & UI",
-  },
-  {
-    name: "Happy Hues",
-    preview: "/previews/happy-hues.png",
-    description:
-      "A curated collection of color palettes and design inspiration for web designers and developers.",
-    link: "https://www.happyhues.co",
-    tags: ["design", "typography"],
-    category: "Design & UI",
   },
   {
     name: "Beekeeper Studio",
@@ -541,15 +425,6 @@ const data: Tool[] = [
     category: "Audio & Video",
   },
   {
-    name: "Penpot",
-    preview: "/previews/penpot.png",
-    description:
-      "An open-source design and prototyping tool for creating user interfaces and interactive designs.",
-    link: "https://penpot.app",
-    tags: ["design", "open-source"],
-    category: "Design & UI",
-  },
-  {
     name: "toolfinder",
     preview: "/previews/toolfinder.png",
     description:
@@ -559,24 +434,6 @@ const data: Tool[] = [
     category: "Productivity & Utilities",
   },
   {
-    name: "Tailwind Buttons - ibelick",
-    preview: "/previews/tailwind-buttons-ibelick.png",
-    description:
-      "A collection of beautifully designed buttons using Tailwind CSS.",
-    link: "https://buttons.ibelick.com",
-    tags: ["dev", "design", "open-source"],
-    category: "Design & UI",
-  },
-  {
-    name: "Flexbox Labs",
-    preview: "/previews/flexbox-labs.png",
-    description:
-      "A collection of tools and resources for working with Flexbox layouts.",
-    link: "https://flexboxlabs.netlify.app",
-    tags: ["dev", "design", "open-source"],
-    category: "Design & UI",
-  },
-  {
     name: "JavaScript Patterns",
     preview: "/previews/javascript-patterns.png",
     description:
@@ -584,15 +441,6 @@ const data: Tool[] = [
     link: "https://www.patterns.dev/",
     tags: ["dev", "docs", "open-source"],
     category: "Documentation & Learning",
-  },
-  {
-    name: "Mesh Gradient Generator",
-    preview: "/previews/mesh-gradient-generator.png",
-    description:
-      "A tool for generating beautiful mesh gradients for your designs.",
-    link: "https://www.learnui.design/tools/mesh-gradient-generator.html",
-    tags: ["dev", "design"],
-    category: "Design & UI",
   },
 ];
 
@@ -617,5 +465,160 @@ export const fontsAndTypography: Tool[] = [
     link: "https://fontshare.com",
     tags: ["font", "typography", "open-source"],
     category: "Fonts & Typography",
+  },
+];
+
+export const designAndUI: Tool[] = [
+  {
+    name: "Lucide Icons",
+    preview: "/previews/lucide.png",
+    description:
+      "Lucide is a collection of open-source icons designed to be simple and consistent.",
+    link: "https://lucide.dev",
+    tags: ["icon", "design", "open-source"],
+    category: "Design & UI",
+  },
+  {
+    name: "Mobbin",
+    preview: "/previews/mobbin.png",
+    description:
+      "A design library with a collection of mobile app designs and patterns.",
+    link: "https://mobbin.com",
+    tags: ["design", "typography"],
+    category: "Design & UI",
+  },
+  {
+    name: "Tailwind CSS Colors",
+    preview: "/previews/tailwind-ui-colors.png",
+    description: "A sample website to sample color palettes for Tailwind CSS.",
+    link: "https://uicolors.app/",
+    tags: ["dev", "design"],
+    category: "Design & UI",
+  },
+  {
+    name: "Carbon",
+    preview: "/previews/carbon.png",
+    description:
+      "A desktop application for creating beautiful images of your source code.",
+    link: "https://carbon.now.sh",
+    tags: ["dev", "design", "open-source", "terminal"],
+    category: "Design & UI",
+  },
+  {
+    name: "Tailwind CSS",
+    preview: "/previews/tailwind-css.png",
+    description:
+      "A utility-first CSS framework for creating modern and responsive web designs.",
+    link: "https://tailwindcss.com",
+    tags: ["dev", "design", "open-source"],
+    category: "Design & UI",
+  },
+  {
+    name: "Boring Avatars",
+    preview: "/previews/boring-avatars.png",
+    description:
+      "A collection of simple, customizable avatars for use in web applications.",
+    link: "https://boringavatars.com",
+    tags: ["dev", "design", "open-source"],
+    category: "Design & UI",
+  },
+  {
+    name: "Lottie Files",
+    preview: "/previews/lottie-files.png",
+    description:
+      "A library for rendering animations created with Adobe After Effects in web applications.",
+    link: "https://github.com/lottiefiles",
+    tags: ["dev", "design", "open-source"],
+    category: "Design & UI",
+  },
+  {
+    name: "CSS Shadow Palette Generator",
+    preview: "/previews/css-shadow-palette-generator.png",
+    description:
+      "A tool for generating CSS shadow palettes, allowing you to create consistent and visually appealing shadows for your web designs.",
+    link: "https://www.joshwcomeau.com/shadow-palette/",
+    tags: ["dev", "design"],
+    category: "Design & UI",
+  },
+  {
+    name: "CSS Gradient Generator",
+    preview: "/previews/css-gradient-generator.png",
+    description:
+      "A tool for creating and customizing CSS gradients, making it easy to add beautiful gradients to your web designs.",
+    link: "https://www.joshwcomeau.com/gradient-generator/",
+    tags: ["dev", "design"],
+    category: "Design & UI",
+  },
+  {
+    name: "SVG Blob generator",
+    preview: "/previews/svg-blob-generator.png",
+    description:
+      "A tool for generating SVG blobs, allowing you to create unique and visually appealing shapes for your web designs.",
+    link: "https://www.blobmaker.app/",
+    tags: ["dev", "design"],
+    category: "Design & UI",
+  },
+  {
+    name: "Unsplash",
+    preview: "/previews/unsplash.png",
+    description:
+      "A platform for finding and sharing high-quality, free-to-use images for your projects.",
+    link: "https://unsplash.com",
+    tags: ["design"],
+    category: "Design & UI",
+  },
+  {
+    name: "Deck Gallery",
+    preview: "/previews/deck-gallery.png",
+    description:
+      "A collection of beautifully designed decks, slides, keynotes, guidelines, etc.",
+    link: "https://deck.gallery",
+    tags: ["design"],
+    category: "Design & UI",
+  },
+  {
+    name: "Happy Hues",
+    preview: "/previews/happy-hues.png",
+    description:
+      "A curated collection of color palettes and design inspiration for web designers and developers.",
+    link: "https://www.happyhues.co",
+    tags: ["design", "typography"],
+    category: "Design & UI",
+  },
+  {
+    name: "Penpot",
+    preview: "/previews/penpot.png",
+    description:
+      "An open-source design and prototyping tool for creating user interfaces and interactive designs.",
+    link: "https://penpot.app",
+    tags: ["design", "open-source"],
+    category: "Design & UI",
+  },
+  {
+    name: "Tailwind Buttons - ibelick",
+    preview: "/previews/tailwind-buttons-ibelick.png",
+    description:
+      "A collection of beautifully designed buttons using Tailwind CSS.",
+    link: "https://buttons.ibelick.com",
+    tags: ["dev", "design", "open-source"],
+    category: "Design & UI",
+  },
+  {
+    name: "Flexbox Labs",
+    preview: "/previews/flexbox-labs.png",
+    description:
+      "A collection of tools and resources for working with Flexbox layouts.",
+    link: "https://flexboxlabs.netlify.app",
+    tags: ["dev", "design", "open-source"],
+    category: "Design & UI",
+  },
+  {
+    name: "Mesh Gradient Generator",
+    preview: "/previews/mesh-gradient-generator.png",
+    description:
+      "A tool for generating beautiful mesh gradients for your designs.",
+    link: "https://www.learnui.design/tools/mesh-gradient-generator.html",
+    tags: ["dev", "design"],
+    category: "Design & UI",
   },
 ];
