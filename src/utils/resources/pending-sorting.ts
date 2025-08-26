9d05bf47385343beb21171c4fdb8f9a067f01fcf
Andres-XL <amovillaw01@gmail.com>
2025-08-25 21:13:50 -0500

    5 more tools

--- src/utils/resources/pending-sorting.ts
+++ src/utils/resources/pending-sorting.ts
@@ -1,7 +1,41 @@
 import type { Resource } from '../types';
 
 const pendingSortTools: Resource[] = [
-  // Tools will be added here as they are discovered and need to be sorted
+  {
+    name: 'Easing Graphs',
+    preview: 'easing-graphs.png',
+    link: 'https://www.easing.dev/',
+    description: 'A collection of easing graphs for animations',
+    tags: ['dev'],
+  },
+  {
+    name: 'Magic UI',
+    preview: 'magic-ui.png',
+    link: 'https://magicui.design/',
+    description: 'A collection of magical UI components',
+    tags: ['dev', 'design'],
+  },
+  {
+    name: 'All ShadCN',
+    preview: 'all-shadcn.png',
+    link: 'https://www.shadcn.com/',
+    description: 'A collection of Shadcn templates, components, and more.',
+    tags: ['dev', 'design'],
+  },
+  {
+    name: 'Crosspost',
+    preview: 'crosspost.png',
+    link: 'https://github.com/humanwhocodes/crosspost/',
+    description: 'A tool for cross-posting content across platforms',
+    tags: ['open-source'],
+  },
+  {
+    name: 'Layouts.dev',
+    preview: 'layouts-dev.png',
+    link: 'https://www.layouts.dev/',
+    description: 'An editor for building Tailwind interfaces',
+    tags: ['dev', 'design'],
+  },
 ];
 
 export { pendingSortTools };
